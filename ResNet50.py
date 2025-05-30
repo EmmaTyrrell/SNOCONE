c4af3a7062b66703f6077cd420feeb59f8b28022
Emma Tyrrell <126344950+EmmaTyrrell@users.noreply.github.com>
2025-05-30 11:36:41 -0600

    Create ResNet50.py
    This is a ResNet50 architecure.

---
+++ ResNet50.py
@@ -0,0 +1,35 @@
+def ResNet50(input_shape, num_classes, final_activation):
+    """ResNet-50 implementation using bottleneck blocks."""
+    inputs = Input(shape=input_shape)
+    
+    # Initial convolution
+    x = Conv2D(64, 7, strides=2, padding='same', name='conv1_conv')(inputs)
+    x = BatchNormalization(axis=3, epsilon=1.001e-5, name='conv1_bn')(x)
+    x = Activation('relu', name='conv1_relu')(x)
+    x = tf.keras.layers.MaxPooling2D(3, strides=2, padding='same', name='pool1_pool')(x)
+
+    # Residual blocks with bottleneck design
+    x = conv_block(x, 64, name='conv2_block1')
+    x = identity_block(x, 64, name='conv2_block2')
+    x = identity_block(x, 64, name='conv2_block3')
+
+    x = conv_block(x, 128, stride=2, name='conv3_block1')
+    for i in range(2, 5):
+        x = identity_block(x, 128, name=f'conv3_block{i}')
+
+    x = conv_block(x, 256, stride=2, name='conv4_block1')
+    for i in range(2, 7):
+        x = identity_block(x, 256, name=f'conv4_block{i}')
+
+    x = conv_block(x, 512, stride=2, name='conv5_block1')
+    x = identity_block(x, 512, name='conv5_block2')
+    x = identity_block(x, 512, name='conv5_block3')
+
+    # Global average pooling and final layers
+    x = GlobalAveragePooling2D(name='avg_pool')(x)
+    x = Dense(1024, activation='relu', name='fc1')(x)
+    x = Dropout(0.1, name='dropout1')(x)
+    outputs = Dense(num_classes, activation=final_activation, name='predictions')(x)
+
+    model = Model(inputs, outputs, name='resnet50')
+    return model
