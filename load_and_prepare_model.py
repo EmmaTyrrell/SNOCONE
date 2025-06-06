921a528a38ab70c781e82451d05c5032ce8dd4b7
Emma Tyrrell <126344950+EmmaTyrrell@users.noreply.github.com>
2025-06-06 13:43:44 -0600

    Update load_and_prepare_model.py

--- load_and_prepare_model.py
+++ load_and_prepare_model.py
@@ -1,3 +1,7 @@
+import tensorflow as tf
+from keras.models import Sequential, Model
+from tensorflow.keras.models import load_model
+
 def load_and_prepare_model(model_path):
     """Load a saved Keras model"""
     model = tf.keras.models.load_model(model_path)
