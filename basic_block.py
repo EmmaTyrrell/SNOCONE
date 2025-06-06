e58d349ffb22b21b07dbdd1b1a7f6651a32fb31b
Emma Tyrrell <126344950+EmmaTyrrell@users.noreply.github.com>
2025-06-06 13:38:27 -0600

    Update basic_block.py

--- basic_block.py
+++ basic_block.py
@@ -1,3 +1,21 @@
+import numpy as np
+from tensorflow.keras.losses import MeanSquaredError, MeanAbsoluteError
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.utils import register_keras_serializable
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.utils import to_categorical
+from keras.models import Sequential, Model
+from tensorflow.keras.models import load_model
+from keras.layers import Dense, Dropout, Flatten, Lambda, Activation
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D, Dense, BatchNormalization, Activation, Input, Add
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.utils import Sequence
+from tensorflow.keras.utils import register_keras_serializable
+from tensorflow.keras.losses import MeanSquaredError, MeanAbsoluteError
+from tensorflow.keras.losses import Loss
+import gc
+import tensorflow.keras.backend as K
+
 def basic_block(x, filters, stride=1, conv_shortcut=False, name=None):
     """Basic residual block for ResNet-18/34."""
     bn_axis = 3
