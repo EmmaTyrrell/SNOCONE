daf29cdf0a6a6285fa0c2cac2d0571ae370caed7
Emma Tyrrell <126344950+EmmaTyrrell@users.noreply.github.com>
2025-06-06 13:49:55 -0600

    Update save_array_as_raster.py

--- save_array_as_raster.py
+++ save_array_as_raster.py
@@ -1,3 +1,12 @@
+
+import rasterio
+import os
+import numpy as np
+from rasterio.mask import mask
+from rasterio.windows import from_bounds
+from rasterio.transform import from_bounds 
+from rasterio.transform import from_bounds
+
 def save_array_as_raster(output_path, array, extent, crs, nodata_val=-1):
     height, width = array.shape
     transform = from_bounds(*extent, width=width, height=height)
