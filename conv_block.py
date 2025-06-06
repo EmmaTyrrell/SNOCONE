3098ee34e846d3f2daef344de52a0c6825ca6bf9
Emma Tyrrell <126344950+EmmaTyrrell@users.noreply.github.com>
2025-06-06 13:38:38 -0600

    Update conv_block.py

--- conv_block.py
+++ conv_block.py
@@ -1,3 +1,21 @@
+import numpy as np
+from tensorflow.keras.losses import MeanSquaredError, MeanAbsoluteError
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.utils import register_keras_serializable
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.utils import to_categorical
+from keras.models import Sequential, Model
+from tensorflow.keras.models import load_model
+from keras.layers import Dense, Dropout, Flatten, Lambda, Activation
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D, Dense, BatchNormalization, Activation, Input, Add
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.utils import Sequence
+from tensorflow.keras.utils import register_keras_serializable
+from tensorflow.keras.losses import MeanSquaredError, MeanAbsoluteError
+from tensorflow.keras.losses import Loss
+import gc
+import tensorflow.keras.backend as K
+
 def conv_block(x, filters, kernel_size=3, stride=1, conv_shortcut=True, name=None):
     """A residual block with a convolution shortcut."""
     bn_axis = 3  # Channel axis for 'channels_last'
