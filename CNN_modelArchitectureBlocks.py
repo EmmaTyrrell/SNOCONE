0e6a0d075f2ec4b312caed30b2d9331acdd3eb92
Emma Tyrrell <126344950+EmmaTyrrell@users.noreply.github.com>
2025-07-22 09:14:00 -0600

    Update CNN_modelArchitectureBlocks.py

--- CNN_modelArchitectureBlocks.py
+++ CNN_modelArchitectureBlocks.py
@@ -18,7 +18,7 @@ from keras.utils import to_categorical
 from keras.models import Sequential, Model
 from tensorflow.keras.models import load_model
 from keras.layers import Dense, Dropout, Flatten, Lambda, Activation
-from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D, Dense, BatchNormalization, Activation, Input, Add
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, GlobalAveragePooling2D, Dense, BatchNormalization, Activation, Input, Add, DepthwiseConv2D, GlobalAveragePooling2D
 from tensorflow.keras.layers import UpSampling2D, Reshape, Concatenate
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 import time
